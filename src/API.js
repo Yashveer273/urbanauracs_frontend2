79e39913a0e13ea5f47548c62175c0d60c2e536d
Yashveer273 <yash@dekatc.com>
2026-03-11 11:34:52 +0530

    11-3-26.2

--- src/API.js
+++ src/API.js
@@ -8,9 +8,9 @@ import {
   getDocs,
   writeBatch,
 } from "firebase/firestore";
-// export const API_BASE_URL = "https://urbanaurabzcs.xyz";
+export const API_BASE_URL = "https://urbanaurabzcs.xyz";
 // export const API_BASE_URL = "http://localhost:8000";
-export const API_BASE_URL = "http://192.168.1.7:8000";
+// export const API_BASE_URL = "http://192.168.1.7:8000";
 
 export const registorUser = async (
   username,
